a2cbfe03a86d05a947127247e7b09e91ae2445a7
Kamil Straczek <wi21b019@technikum-wien.at>
2022-06-21 16:58:52 +0200

    Initial commit

---
+++ src/main/java/technikum/at/calculatortest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package technikum.at.calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/technikum/at/calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package technikum.at.calculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
